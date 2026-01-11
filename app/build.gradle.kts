726625cf962de3abd91f36bb7625f3fe374f1eeb
codetosustain <ananyapihu3912@gmail.com>
2026-01-12 01:52:33 +0530

    ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.example.nutrishield_1"
         minSdk = 24
-        targetSdk = 35
+        //noinspection OldTargetApi
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
